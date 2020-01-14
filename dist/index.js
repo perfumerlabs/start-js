d275bda0577ad9a532583f52889c86e668f63d7d
Kuanysh <kumold.ino@gmail.com>
2020-01-14 16:27:35 +0600

    export themes

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,14 @@
 'use strict';
 import Page from "./Page";
-import router from "./router"
+import router from "./router";
+import themes from './configs/themes';
 
 let exports = {};
 
 exports.Page = Page;
 
 exports.router = router;
+
+exports.themes = themes;
 
 export default exports;
